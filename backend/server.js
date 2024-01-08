d58715f980707f40f9818b52f021336db1e95be2
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-08 15:08:34 +0100

    Try to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const mysql = require('mysql');
 const cors = require('cors');
 const express = require('express');
 
@@ -7,16 +6,8 @@ const app = express();
 
 app.use(cors());
 
-app.use(cors(
-    {
-        origin: ["mern-stack-zeta.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-app.use(express.json())
 
-mongoose.connect('mongodb+srv://andreavicari77:<password>@mern-vercel.1iyqseq.mongodb.net/?retryWrites=true&w=majority');
+app.use(express.json())
 
 
 
