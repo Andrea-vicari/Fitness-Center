eb139bf53c059ee62f7c8c46e13bae1de696e81f
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-08 15:41:13 +0100

    Added mongoose and mongo connenction

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose')
 
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://mern-stack-gp2j.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 
 app.use(express.json())
-
+mongoose.connect('mongodb+srv://andreavicari77:DuRIkZ0u0redNhqB@mern-vercel.1iyqseq.mongodb.net/?retryWrites=true&w=majority');
 
 
 app.get('/', (re,res)=>{
