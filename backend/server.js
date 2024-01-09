cee397077cbb780368f35c93f2556d01884c7fb4
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-09 14:38:19 +0100

    Tolto app use cors

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,6 @@ const workoutsRoutes = require('./routes/workouts');
 const app = express();
 
 
-app.use(cors(
-    {
-        origin: ["https://mern-stack-gp2j.vercel.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         app.listen(process.env.PORT, () => console.log(`Connected to DB and Listening on port ${process.env.PORT}`));
