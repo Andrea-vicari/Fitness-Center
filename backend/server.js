04504118bdad9111df55a1abf0d341b0bf796e80
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-09 16:18:26 +0100

    Provate le due veriosni proma che

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,24 @@
 const cors = require('cors');
 const express = require('express');
 require ('dotenv').config();
-const mongoose = require('mongoose')
-
-const workoutsRoutes = require('./routes/workouts');
-
 
 const app = express();
 
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        app.listen(process.env.PORT, () => console.log(`Connected to DB and Listening on port ${process.env.PORT}`));
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-
-app.use((req, res, next)=>{
-    console.log(req.path, req.method)
-    next()
-})
+app.use(cors({
+    origin: ["https://mern-stack-gp2j.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 
 app.use(express.json())
 
-app.use('/api/workouts', workoutsRoutes)
 
 
+app.get('/', (re,res)=>{
+
+    return res.json("From the BackEnd side works fine!!")
+});
+
+
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
