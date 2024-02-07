0559a08c708dbab5e3ba0a4feaf6925312fdead2
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-07 08:37:17 +0100

    Deleted find from allWorkouts

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -17,7 +17,6 @@ export const useLogin = () =>{
             body: JSON.stringify({email, password}),
             headers:{
                 'Content-Type': 'application/json'
-
               }
 
         })
