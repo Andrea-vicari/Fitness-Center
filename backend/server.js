8c6020ccf86fa6cfe6b1374260b341e10145fe51
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-13 14:51:46 +0100

    IMPORTANT: Change the app.user CORS url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: ["https://fitness-center-client.vercel.app/"],
         methods: ["POST", "GET", "PUT", "PATCH"],
         credentials: true
     }
