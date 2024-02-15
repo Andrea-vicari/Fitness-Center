05d8f6e800da542a98a426193c00fd7cf76e9591
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-15 13:59:42 +0100

    Deleted the FAILED trial from ReactRDOM

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,22 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthContextProvider } from './context/AuthContext.jsx';
-import Root from "./routes/root";
-import ErrorPage from "./errorpage.jsx";
+import { BrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-     <AuthContextProvider>
-         <RouterProvider router={router} />
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+      <BrowserRouter>
+      <AuthContextProvider>
+
+          <App />
+
+      </AuthContextProvider>
+      </BrowserRouter>
+
+
+)
