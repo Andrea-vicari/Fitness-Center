b4092b21571b4d3bc2c0f0468ac08f8c90e3f800
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-20 14:16:03 +0100

    Timer

--- frontend/src/Components/Timer.jsx
+++ frontend/src/Components/Timer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Timer() {
   return (
     <div className="modal-body py-3 text-white">
-        <h4 className="text-white mt-3 fw-bold">00:00:00</h4>
+        <h1 className="text-center text-white mt-3 fw-bold">00:00:00</h1>
     </div>
   )
 }
