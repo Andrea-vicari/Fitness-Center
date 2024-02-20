8a0a7fed6bc4b3f68c3367200e1d9f146bcf9b00
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-20 14:22:09 +0100

    On going Timer

--- frontend/src/Components/Timer.jsx
+++ frontend/src/Components/Timer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Timer() {
+function Timer(props) {
   return (
     <div className="modal-body py-3 text-white">
-        <h1 className="text-center text-white mt-3 fw-bold">00:00:00</h1>
+        <div className="timer">
+            <span className="digits">
+                {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
+            </span>
+            <span className="digits">
+                {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
+            </span>
+            <span className="digits mili-sec">
+                {("0" + ((props.time / 10) % 100)).slice(-2)}
+            </span>
+        </div>
     </div>
   )
 }
