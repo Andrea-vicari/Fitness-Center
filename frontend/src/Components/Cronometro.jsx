92ff1f531c19057dca72cd39c136e20ce2fa7dfd
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-20 17:21:14 +0100

    Crono header

--- frontend/src/Components/Cronometro.jsx
+++ frontend/src/Components/Cronometro.jsx
@@ -12,13 +12,13 @@ function Cronometro() {
 
   return (
 
-              <div className="modal modal-sheet bg-dark px-4 py-md-5" tabIndex="-1" role="dialog" id="cronometro">
+              <div className="modal-fade modal-sheet bg-dark px-4 py-md-5" tabIndex="-1" role="dialog" id="cronometro">
               <div className="modal-dialog-centered modal-xl bg-dark" role="document">
                 <div className="modal-content rounded-4 shadow bg-dark" >
                   <div className="modal-header d-flex justify-content-between">
                   <img src={logo} width="50"></img>
                     <h2 className="modal-title text-white">CRONOMETRO</h2>
-                    <i className='fa fa-times px-2 fs-4' onClick={()=>closeCrono()}></i>Chiudi
+                    <i className='fa fa-times px-2 fs-4 text-danger' onClick={()=>closeCrono()}></i>Chiudi
 
                   </div>
                   <Timer />
