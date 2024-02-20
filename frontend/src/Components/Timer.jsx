44983b2cdb5779d4a4f73770a4651290cbfe3427
Andrea-vicari <andrea.vicari77@gmail.com>
2024-02-20 17:42:00 +0100

    Ok almost done the Cronometro

--- frontend/src/Components/Timer.jsx
+++ frontend/src/Components/Timer.jsx
@@ -4,13 +4,13 @@ function Timer(props) {
   return (
     <div className="modal-body py-3 text-white">
         <div className="timer">
-            <span className="digits">
+            <span className="fs-2 text-white">
                 {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
             </span>
-            <span className="digits">
+            <span className="fs-2 text-white">
                 {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
             </span>
-            <span className="digits mili-sec">
+            <span className="fs-2 text-white">
                 {("0" + ((props.time / 10) % 100)).slice(-2)}
             </span>
         </div>
