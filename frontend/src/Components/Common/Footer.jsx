7520f1148d09baa1b315844d9efdb90b3d520ed5
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-12 08:08:24 +0200

    Update Footer.jsx

--- frontend/src/Components/Common/Footer.jsx
+++ frontend/src/Components/Common/Footer.jsx
@@ -9,9 +9,9 @@ return(
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center pt-3 mt-4 border-top bg-dark">
                 <div className="col-md-4 d-flex align-items-center">
-                <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                    <img src={logo} style={{width:50}}/>
-                </a>
+          <Link className="navbar-brand d-flex" to={'/'}>
+            <img src={logo} style={{width:150}}/>
+          </Link>
                 <span className="mb-3 mb-md-0 text-white-50">© 2024 Fitness Club</span>
                 </div>
 
@@ -20,6 +20,9 @@ return(
                 <li className="ms-3"><a className="text-body-secondary" href="#"><i className=" fs-4 text-white fab fa-facebook"></i></a></li>
                 <li className="ms-3"><a className="text-body-secondary" href="#"></a></li>
                 </ul>
+          <button id="myBtn" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}} className='btn btn-sm btn-outline-secondary'>
+            <i className="bi bi-arrow-bar-up"></i>
+          </button>
             </footer>
         </div>
 
